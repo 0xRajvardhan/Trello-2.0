98ca5bdb244ece90bc0251d52c09bde8dcbdfdfb
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-06-05 13:12:30 +0530

    :fire: add: HEADER

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 
 import Image from "next/image";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Header from "@/Components/Header";
 
-
 export default function Home() {
   return (
     <main>
