513fa0d8163530fd03946780f5247a72ae9dbb7c
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-06-07 23:24:08 +0530

    :white_check_mark: appwrite cloud setup

---
+++ Components/Board.tsx
@@ -0,0 +1,13 @@
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+
+function Board() {
+  return (
+    <DragDropContext>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) => <div>{/* rendering all the columns */}</div>}
+      </Droppable>
+    </DragDropContext>
+  );
+}
+
+export default Board;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
+import Board from "@/Components/Board";
 import Header from "@/Components/Header";
 
 export default function Home() {
   return (
     <main>
-      {/* Header  */}
       <Header />
-      {/* Board  */}
-      <h1>Hello Trello 2.0 AI Clone</h1>
+
+      <Board />
     </main>
   );
 }
