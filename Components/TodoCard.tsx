92352e033f76586d0aa27bd313663d49751a1377
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-06-11 08:40:01 +0530

    :sparkles: built: todo component

--- Components/TodoCard.tsx
+++ Components/TodoCard.tsx
@@ -1,3 +1,4 @@
+import { XCircleIcon } from "@heroicons/react/24/solid";
 import {
   DraggableProvidedDragHandleProps,
   DraggableProvidedDraggableProps,
@@ -20,7 +21,24 @@ function TodoCard({
   draggableProps,
   dragHandleProps,
 }: Props) {
-  return <div>TodoCard</div>;
+  return (
+    <div
+      className="bg-white rounded-md space-y-2 drop-shadow-md"
+      {...draggableProps}
+      {...dragHandleProps}
+      ref={innerRef}
+    >
+      <div className="flex justify-between items-center p-5">
+        <p>{todo.title}</p>
+        <button className="text-red-500 hover:text-red-600">
+          <XCircleIcon className="ml-5 h-8 w-8"/>
+        </button>
+      </div>
+
+      {/* Add image here  */}
+
+    </div>
+  );
 }
 
 export default TodoCard;
